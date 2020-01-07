1ee41d8b650fd4d78b323d74acfb4e0e79a77e01
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-07 02:04:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,7 @@ class TicTacToe
   end
   
   def valid_move?(index)
-    if input_to_index(index) == "X" || input_to_index == "O"
+    if input_to_index(index) == "X" || input_to_index(index) == "O"
           true
         else 
           false 
