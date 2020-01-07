e8db53bb27e3d9288136583f3abe6c9564c544a6
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-07 01:33:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ class TicTacToe
   end 
   
   def position_taken?(index) 
-        if index == "X" || index == "O"
+        if input_to_index == "X" || input_to_index == "O"
           true
         else 
           false 
